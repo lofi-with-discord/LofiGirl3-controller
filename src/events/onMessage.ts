a3440252a6a7d3b1964fbca0bdc635cc0f8cb8e4
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-08-09 14:57:58 +0900

    feat: add deprecated message for legacy commands

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -1,68 +1,16 @@
-import { get } from 'superagent'
-import { Locale } from '../types'
 import { Message } from 'discord.js'
-import Query from '../structures/Query'
-import Client from '../structures/BotClient'
-import registUser from '../functions/registUser'
-import I18nParser from '../structures/I18nParser'
-import PlayerClient from '../structures/PlayerClient'
-import { DefaultEmbed, hasPermissions } from '../utils'
-import DatabaseClient from '../structures/DatabaseClient'
+import BotClient from '../structures/BotClient'
 
-const voteCache = [] as string[]
+export default function onMessage (client: BotClient, msg: Message) {
+  if (!msg.content.startsWith(client.prefix)) return
 
-export default function onMessage (db: DatabaseClient, i18n: I18nParser, player: PlayerClient) {
-  return async function (client: Client, msg: Message) {
-    if (!client.user) return
-    if (msg.author.bot) return
-
-    if (msg.channel.type === 'dm') {
-      msg.channel.send(i18n.__l('dm_disallow').join('\n'))
-      return
-    }
-
-    if (msg.mentions.has(client.user, { ignoreEveryone: true, ignoreRoles: true })) {
-      const m = await msg.channel.send(i18n.__l('mention_call_1').join('\n'))
-      setTimeout(() => m.edit(i18n.__l('mention_call_2').join('\n').replace(/%1\$s/g, client.prefix)), 500)
-      return
-    }
-
-    if (!msg.content.startsWith(client.prefix)) return
-    if (!hasPermissions(client.user, msg.channel, ['SEND_MESSAGES'])) return
-
-    const query = new Query(client.prefix, msg.content)
-    const target = client.commands.find((command) => command.aliases.includes(query.cmd))
-
-    if (!target) return
-
-    let userData = await db.getUserData(msg.author)
-    if (!userData) userData = await registUser(client, msg, db, i18n)
-
-    const locale: Locale = (phrase: string, ...args: any[]) =>
-      i18n.__({ locale: userData?.locale, phrase }, ...args)
-
-    locale.i18n = i18n
-
-    if (!client.koreanbots) return target.default(client, msg, query, locale, db, player)
-    if (voteCache.includes(msg.author.id)) return target.default(client, msg, query, locale, db, player)
-
-    const { status, body } = await get(`https://koreanbots.dev/api/v2/bots/${client.user.id}/vote?userID=${msg.author.id}`)
-      .set('Authorization', client.koreanbots)
-      .catch((err) => { console.log(err); return { status: 400, body: { } } })
-
-    if (status !== 200 || body?.data?.voted) return target.default(client, msg, query, locale, db, player)
-
-    const perm2 = hasPermissions(client.user, msg.channel, ['EMBED_LINKS'])
-    if (!perm2) return msg.channel.send(locale('give_me_heart_no_embed', `https://koreanbots.dev/bots/${client.user.id}`))
-
-    const embed = new DefaultEmbed('welcome', null, {
-      title: locale('give_me_heart_title'),
-      description:
-        locale('give_me_heart_description') + '\n\n' +
-        locale('give_me_heart_button_1', `https://koreanbots.dev/bots/${client.user.id}`) + ' • ' +
-        locale('give_me_heart_button_2', 'https://github.com/lofi-with-discord/controller')
-    })
-
-    msg.channel.send(embed)
-  }
+  msg.channel.send(
+    `\`${client.prefix}\`를 사용한 명령어는 지원 종료 되었습니다. \`/\`를 사용해 주세요\n` +
+    '만약 `/` 명령어가 보이지 않을 경우 빗금명령어 권한을 확인하거나 다음 주소를 통해 다시 초대해 보세요: \n' +
+    `https://discord.com/api/oauth2/authorize?client_id=${client.user?.id}&permissions=0&scope=applications.commands%20bot\n` +
+    '\n' +
+    `legacy (\`${client.prefix}\`) commands have been deprecated. Please use \`/\` instead.\n` +
+    'if `/` commands are not displayed, check your permissions or use the following link to invite again: \n' +
+    `https://discordapp.com/api/oauth2/authorize?client_id=${client.user?.id}&permissions=0&scope=applications.commands%20bot\n`
+  )
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import onReady from './events/onReady'
 import onMessage from './events/onMessage'
 import Client from './structures/BotClient'
-import I18nParser from './structures/I18nParser'
-import PlayerClient from './structures/PlayerClient'
-import DatabaseClient from './structures/DatabaseClient'
 
 const client = new Client()
-const i18n = new I18nParser()
-const db = new DatabaseClient()
-const player = new PlayerClient()
 
-client.registEvent('ready', onReady)
-client.registEvent('message', onMessage(db, i18n, player))
+client.registEvent('message', onMessage)
